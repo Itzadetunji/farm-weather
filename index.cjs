6d1d8028a78941e7763bf1b28efb0c4bbbf4366b
Adetunji Adeyinka <adetunjiadeyinka29@gmail.com>
2024-01-23 21:20:49 +0100

    Feat: Changed port

--- index.cjs
+++ index.cjs
@@ -13,7 +13,7 @@ app.use(express.static(__dirname + "/views"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
 	console.log(`Server is running on port ${PORT}`);
